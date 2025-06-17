fcb647ba98e9f369f493087e1c65a332a815f155
aliyevfexri <aliyevfexri@yandex.com>
2025-06-17 11:41:49 +0400

    increase upload timeout

--- Sources/NetCore/Clients/FileUploaderClient.swift
+++ Sources/NetCore/Clients/FileUploaderClient.swift
@@ -59,7 +59,12 @@ public class FileUploaderClient {
         signedRequest.httpBody = nil
         
         guard let uploadData else { return .failure(NetCoreError.backendError("No data to upload")) }
-        let result = try? await URLSession.shared.upload(for: signedRequest, from: uploadData, delegate: delegate)
+        
+        let session = URLSession.shared
+        session.configuration.timeoutIntervalForRequest = 300
+        session.configuration.timeoutIntervalForResource = 300
+        
+        let result = try? await session.upload(for: signedRequest, from: uploadData, delegate: delegate)
         
         if let result {
             if let handledError = JSONResponseHandler().handle(with: result.0, and: result.1) {
